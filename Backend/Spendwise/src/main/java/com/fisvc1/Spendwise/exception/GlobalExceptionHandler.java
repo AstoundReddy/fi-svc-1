ca7dae26d56400096c8d45bfd71e8611660ecaa0
Astound <aryachandanreddy@gmail.com>
2024-04-20 15:30:45 +0530

    backend done

--- Backend/Spendwise/src/main/java/com/fisvc1/Spendwise/exception/GlobalExceptionHandler.java
+++ Backend/Spendwise/src/main/java/com/fisvc1/Spendwise/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fisvc1.Spendwise.exception;
 
 import com.fisvc1.Spendwise.exception.ResourceNotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -19,6 +20,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleTokenExpiry(MissingServletRequestParameterException ex) {
+        String name = ex.getParameterName();
+        return new ResponseEntity<>("'" + name + "' parameter is missing", HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<String> handleMissingParams(MissingServletRequestParameterException ex) {
         String name = ex.getParameterName();
         return new ResponseEntity<>("'" + name + "' parameter is missing", HttpStatus.BAD_REQUEST);
